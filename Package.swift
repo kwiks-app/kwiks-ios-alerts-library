27e281d99727aa210b28f9d80a156d4072c436ee
Eric Solberg <eric.jacob.solberg@gmail.com>
2023-12-15 02:37:33 -0700

    Changed name

--- Package.swift
+++ Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "KwiksAlerts",
+    name: "kwiks-ios-alerts-library",
     platforms: [
         .iOS(.v13) // Minimum supported iOS version
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "KwiksAlerts",
-            targets: ["KwiksAlerts"]),
+            name: "kwiks-ios-alerts-library",
+            targets: ["kwiks-ios-alerts-library"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "KwiksAlerts"),
+            name: "kwiks-ios-alerts-library"),
         .testTarget(
             name: "KwiksAlertsTests",
-            dependencies: ["KwiksAlerts"]),
+            dependencies: ["kwiks-ios-alerts-library"]),
     ]
 )
